89069d326e56d8699d67a44898131753f2765667
Expert <bekzodmuxtorxonov@gmail.com>
2023-10-28 08:02:41 +0500

    eight commit

--- app/src/main/java/com/example/handybook/ui/MainFragment.kt
+++ app/src/main/java/com/example/handybook/ui/MainFragment.kt
@@ -1,19 +1,23 @@
 package com.example.handybook.ui
 
-import android.annotation.SuppressLint
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.handybook.R
 import com.example.handybook.databinding.FragmentMainBinding
+import com.google.android.material.navigation.NavigationView
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), NavigationView.OnNavigationItemSelectedListener {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -25,6 +29,7 @@ class MainFragment : Fragment() {
         }
     }
 
+    private var drawerLayout: DrawerLayout? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,6 +39,21 @@ class MainFragment : Fragment() {
         parentFragmentManager.beginTransaction()
             .add(R.id.container, BoshSahifaFragment())
             .commit()
+
+        drawerLayout = binding.drawerLayout
+        val navigationView = binding.navView
+        val toolbar = binding.toolbar
+
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            requireActivity(), binding.drawerLayout, toolbar, R.string.open_nav,
+            R.string.close_nav
+        )
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
 
 
         return binding.root
@@ -48,5 +68,9 @@ class MainFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
